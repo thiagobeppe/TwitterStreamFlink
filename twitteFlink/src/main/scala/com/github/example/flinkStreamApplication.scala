d01d26f42fdc7fda912849c0deb709879e7b3588
thiagobeppe <thiagoabeppe@gmail.com>
2020-04-24 08:37:33 -0300

    Connecting to kafka

--- twitteFlink/src/main/scala/com/github/example/flinkStreamApplication.scala
+++ twitteFlink/src/main/scala/com/github/example/flinkStreamApplication.scala
@@ -1,10 +1,20 @@
 package com.github.example
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import java.util.Properties
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 
 object flinkStreamApplication {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "localhost:9092")
+    val stream = env
+      .addSource(new FlinkKafkaConsumer[String]("TWEET_TOPIC_STREAM", new SimpleStringSchema(), properties))
+      .print()
+    env.execute("Kafka Consumer")
   }
 }
